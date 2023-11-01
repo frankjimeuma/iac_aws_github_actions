ae54eb4b91af771a5787317e73e63979c7a1ee72
frankjimeuma <frankjimeuma@gmail.com>
2023-11-01 15:37:33 -0600

    to add bucket for state files

--- main.tf
+++ main.tf
@@ -1,10 +1,37 @@
+terraform {
+  backend "s3" {
+    bucket                  = "s3-terraform-frank-state"
+    key                     = "my-terraform-project"
+    region                  = "us-east-1"
+
+  }
+}
+
+
+
 provider "aws" {}
 
-resource "aws_instance" "example" {
-    ami           = "ami-0fc5d935ebf8bc3bc"
-    instance_type = "t2.micro"
-
-    tags = {
-        Name = "linux_aws_github_actions"
-    }
+resource "aws_instance" "frank_instance" {
+  ami           = "ami-053b0d53c279acc90"  # ubuntu AMI
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.frank_kp.key_name
+  subnet_id = aws_subnet.subnet_frank_public.id
+  vpc_security_group_ids = [aws_security_group.security_group04.id]
+  user_data = <<EOF
+#!/bin/bash
+sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+cd /var/www/
+sudo chgrp ubuntu html
+sudo chown ubuntu html
+cd html
+wget https://iac-frank.s3.amazonaws.com/binaries.zip
+sudo apt-get install unzip
+unzip binaries.zip
+EOF
+  tags = {
+    Name = "PublicInstance"
+  }
 }
---
+++ security.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "security_group04" {
+  name        = var.securitygroup_name
+  description = "Demo Security Group"
+   vpc_id      = aws_vpc.vpc_frank.id
+
+  ingress {
+    description      = "SSH Access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["152.231.0.0/16"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Nginx Access"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["152.231.0.0/16"]
+    # ipv6_cidr_blocks = ["::/0"]
+  }
+
+   egress {
+     from_port        = 0
+     to_port          = 0
+     protocol         = "-1"
+     cidr_blocks      = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+  }
+
+   tags = {
+     Name = var.securitygroup_name
+   }
+}
+
+ resource "aws_key_pair" "frank_kp" {
+   key_name   = "frank_keypair"
+   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDApSPO88rXtgXbAclPrwdIgejtmD69mc6oVc0zridBXnJYHyZJWMI0Bq/avYC5C0PcAjmOsCHwC7T83sHzrIxgozBaQKNf88wgHgKVHXP+ACA14Ew1F/sQD+fHCY3CTtHPd0IDyweFH7yB8sR6aVHDzBI/RDAAs+EPOG7lIpl9x1zX+771sV6JofWdvkroyXO8H5RwnijsfNpRjJLzMBYasi4tU/j2M8LHfsuf3QxuXkU53boEyBrhSpJ1n6cCeFHvPXSIDhoLoHADZdB7XhF6IHG7/VwEz2Jk30rfhr9A6abnwXiDK6cL57nuEcW1knIuE4EQOFs77iNw2H2paBnIm3G3rcDgrJHH2IJAesE7H3M+PFN2Zh+UImp6juvw0HQ023Su7XAPV6RP4tmnrlk2pGa2uV5OIOXPNGrwDVmvSlRrzS6b2y5Ovw3vpIRX26HeDz4bIMGjPN3ke3Qt3wWSJ/BDEDR+s19/VCPjpQ4cEPZkpGqrpxP0Gr1fsVSwvuc= Administrator@WIN-NUO83NOC74O"
+}
