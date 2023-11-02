1ae973522cd15b9a7860f099df75e28e8ac1d4bb
frankjimeuma <frankjimeuma@gmail.com>
2023-11-02 08:46:37 -0600

    added nat gateway for private subnet

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "vpc_frank" {
   cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "DemoClase04"
+    Name = "iac_aws_github_actions"
   }
 }
 
@@ -31,8 +31,49 @@ resource "aws_internet_gateway" "igw_frank" {
   vpc_id = aws_vpc.vpc_frank.id
 
   tags = {
-    Name = "DemoClase04-igw"
+    Name = "iac_aws_github_actions-igw"
   }
+}
+
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.subnet_frank_private.id
+
+  tags = {
+    Name = "iac_aws_github_actions-nat"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+
+
+
+
+
+resource "aws_route_table" "rt_frank_private" {
+  vpc_id = aws_vpc.vpc_frank.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "PrivateRouteTableNAT"
+  }
+}
+
+
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.subnet_frank_private.id
+  route_table_id = aws_route_table.rt_frank_private.id
+  
+
 }
 
 
