4b22a9f2524cd12358cd6eec9ea772c21fb62306
frankjimeuma <frankjimeuma@gmail.com>
2023-11-01 21:09:48 -0600

    to get or donwload key pair

--- security.tf
+++ security.tf
@@ -47,4 +47,9 @@ ingress {
  resource "aws_key_pair" "frank_kp" {
    key_name   = "frank_keypair"
    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDApSPO88rXtgXbAclPrwdIgejtmD69mc6oVc0zridBXnJYHyZJWMI0Bq/avYC5C0PcAjmOsCHwC7T83sHzrIxgozBaQKNf88wgHgKVHXP+ACA14Ew1F/sQD+fHCY3CTtHPd0IDyweFH7yB8sR6aVHDzBI/RDAAs+EPOG7lIpl9x1zX+771sV6JofWdvkroyXO8H5RwnijsfNpRjJLzMBYasi4tU/j2M8LHfsuf3QxuXkU53boEyBrhSpJ1n6cCeFHvPXSIDhoLoHADZdB7XhF6IHG7/VwEz2Jk30rfhr9A6abnwXiDK6cL57nuEcW1knIuE4EQOFs77iNw2H2paBnIm3G3rcDgrJHH2IJAesE7H3M+PFN2Zh+UImp6juvw0HQ023Su7XAPV6RP4tmnrlk2pGa2uV5OIOXPNGrwDVmvSlRrzS6b2y5Ovw3vpIRX26HeDz4bIMGjPN3ke3Qt3wWSJ/BDEDR+s19/VCPjpQ4cEPZkpGqrpxP0Gr1fsVSwvuc= Administrator@WIN-NUO83NOC74O"
+
+
+   provisioner "local-exec" { # Create a "frank_keypair.pem" to your computer!!
+     command = "echo ${aws_key_pair.frank_kp.private_key} > frank_keypair.pem"
+   }
 }
